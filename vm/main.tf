995fc0a35bbe47ed69da17ee09fcf9a7497ad247
unknown <darshanthenge03@gmail.com>
2025-12-22 14:55:40 +0000

    Initial Terraform Azure modules

---
+++ vm/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = var.vm_size
+  admin_username      = "azureuser"
+
+  network_interface_ids = [var.nic_id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
---
+++ vm/variables.tf
@@ -0,0 +1,6 @@
+variable "vm_name" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "vm_size" {}
+variable "nic_id" {}
+variable "ssh_public_key" {}
