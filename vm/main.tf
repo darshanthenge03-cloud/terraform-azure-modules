1c6632235947f095a45637738ec482c02ba2415d
unknown <darshanthenge03@gmail.com>
2025-12-23 11:02:27 +0000

    VM module creates VNet, Subnet, NIC and VM

--- vm/main.tf
+++ vm/main.tf
@@ -1,16 +1,49 @@
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.vm_name}-vnet"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.vnet_address_space
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.vm_name}-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_address_prefix
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
   location            = var.location
   resource_group_name = var.resource_group_name
   size                = var.vm_size
-  admin_username      = "azureuser"
 
-  network_interface_ids = [var.nic_id]
+  admin_username = var.admin_username
 
   admin_ssh_key {
-    username   = "azureuser"
+    username   = var.admin_username
     public_key = var.ssh_public_key
   }
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
 
   os_disk {
     caching              = "ReadWrite"
--- vm/variables.tf
+++ vm/variables.tf
@@ -2,5 +2,13 @@ variable "vm_name" {}
 variable "location" {}
 variable "resource_group_name" {}
 variable "vm_size" {}
-variable "nic_id" {}
+variable "admin_username" {}
 variable "ssh_public_key" {}
+
+variable "vnet_address_space" {
+  default = ["10.0.0.0/16"]
+}
+
+variable "subnet_address_prefix" {
+  default = ["10.0.1.0/24"]
+}
