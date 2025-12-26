c01c929504706d09147a491c94521fd72e26c3bb
unknown <darshanthenge03@gmail.com>
2025-12-26 13:39:44 +0000

    Add reusable VNet to network module

--- network/main.tf
+++ network/main.tf
@@ -1,57 +1,6 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
+  name                = "vnet-${var.resource_group_name}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = var.vnet_address_space
-}
-
-# Public subnets
-resource "azurerm_subnet" "public" {
-  for_each             = var.public_subnets
-  name                 = each.key
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [each.value]
-}
-
-# Private subnets
-resource "azurerm_subnet" "private" {
-  for_each             = var.private_subnets
-  name                 = each.key
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [each.value]
-}
-
-# Bastion subnet
-resource "azurerm_subnet" "bastion" {
-  count                = var.enable_bastion ? 1 : 0
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [var.bastion_subnet_prefix]
-}
-
-# Bastion public IP
-resource "azurerm_public_ip" "bastion" {
-  count               = var.enable_bastion ? 1 : 0
-  name                = "${var.vnet_name}-bastion-pip"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-# Bastion host
-resource "azurerm_bastion_host" "bastion" {
-  count               = var.enable_bastion ? 1 : 0
-  name                = "${var.vnet_name}-bastion"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                 = "bastion-ipconfig"
-    subnet_id            = azurerm_subnet.bastion[0].id
-    public_ip_address_id = azurerm_public_ip.bastion[0].id
-  }
+  address_space       = [var.vnet_cidr]
 }
--- network/variables.tf
+++ network/variables.tf
@@ -1,4 +1,4 @@
-variable "vnet_name" {
+variable "resource_group_name" {
   type = string
 }
 
@@ -6,28 +6,6 @@ variable "location" {
   type = string
 }
 
-variable "resource_group_name" {
+variable "vnet_cidr" {
   type = string
-}
-
-variable "vnet_address_space" {
-  type = list(string)
-}
-
-variable "public_subnets" {
-  type = map(string)
-}
-
-variable "private_subnets" {
-  type = map(string)
-}
-
-variable "enable_bastion" {
-  type    = bool
-  default = false
-}
-
-variable "bastion_subnet_prefix" {
-  type    = string
-  default = "10.0.100.0/26"
 }
