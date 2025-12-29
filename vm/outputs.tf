d149ee13c5eeb97e23618d18ce25a5a6f65bb547
unknown <darshanthenge03@gmail.com>
2025-12-29 07:19:08 +0000

    Add VM outputs for ID and managed identity

--- vm/outputs.tf
+++ vm/outputs.tf
@@ -5,3 +5,11 @@ output "vm_id" {
 output "vm_principal_id" {
   value = azurerm_linux_virtual_machine.vm.identity[0].principal_id
 }
+
+output "vm_id" {
+  value = var.os_type == "linux" ? azurerm_linux_virtual_machine.linux[0].id : azurerm_windows_virtual_machine.windows[0].id
+}
+
+output "vm_principal_id" {
+  value = var.os_type == "linux" ? azurerm_linux_virtual_machine.linux[0].identity[0].principal_id : azurerm_windows_virtual_machine.windows[0].identity[0].principal_id
+}
