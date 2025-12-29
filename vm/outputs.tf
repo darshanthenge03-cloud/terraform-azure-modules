4239f920a7fc577812f213c53f71d7f74028af9b
unknown <darshanthenge03@gmail.com>
2025-12-29 12:34:04 +0000

    Outputs modified

--- vm/outputs.tf
+++ vm/outputs.tf
@@ -1,12 +1,4 @@
 output "vm_id" {
-  value = azurerm_linux_virtual_machine.vm.id
-}
-
-output "vm_principal_id" {
-  value = azurerm_linux_virtual_machine.vm.identity[0].principal_id
-}
-
-output "vm_id" {
   value = var.os_type == "linux" ? azurerm_linux_virtual_machine.linux[0].id : azurerm_windows_virtual_machine.windows[0].id
 }
 
