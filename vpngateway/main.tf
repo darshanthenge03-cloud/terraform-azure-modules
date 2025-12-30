feb76ffec094dcee7cd44eabc794fd637d1050a5
unknown <darshanthenge03@gmail.com>
2025-12-30 10:57:48 +0000

    main tf and variables tf changed

--- vpngateway/main.tf
+++ vpngateway/main.tf
@@ -1,4 +1,3 @@
-# Public IP for Azure VPN Gateway
 resource "azurerm_public_ip" "vpn" {
   name                = "pip-vpn-gateway"
   location            = var.location
@@ -8,7 +7,6 @@ resource "azurerm_public_ip" "vpn" {
   sku               = "Basic"
 }
 
-# Azure Virtual Network Gateway
 resource "azurerm_virtual_network_gateway" "this" {
   name                = "vnet-gateway"
   location            = var.location
@@ -16,10 +14,10 @@ resource "azurerm_virtual_network_gateway" "this" {
 
   type     = "Vpn"
   vpn_type = "RouteBased"
+  sku      = var.vpn_sku
 
   active_active = false
   enable_bgp    = false
-  sku           = var.vpn_sku
 
   ip_configuration {
     name                          = "vnet-gateway-ipconfig"
@@ -27,29 +25,4 @@ resource "azurerm_virtual_network_gateway" "this" {
     private_ip_address_allocation = "Dynamic"
     subnet_id                     = var.gateway_subnet_id
   }
-}
-
-# Local Network Gateway (On-Prem)
-resource "azurerm_local_network_gateway" "onprem" {
-  name                = var.onprem_gateway_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  gateway_address = var.onprem_public_ip
-  address_space   = var.onprem_address_space
-}
-
-# Site-to-Site VPN Connection
-resource "azurerm_virtual_network_gateway_connection" "s2s" {
-  name                = "s2s-connection"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  type                       = "IPsec"
-  virtual_network_gateway_id = azurerm_virtual_network_gateway.this.id
-  local_network_gateway_id   = azurerm_local_network_gateway.onprem.id
-
-  shared_key = var.shared_key
-
-  enable_bgp = false
 }
--- vpngateway/variables.tf
+++ vpngateway/variables.tf
@@ -8,27 +8,6 @@ variable "location" {
 
 variable "gateway_subnet_id" {
   type = string
-  description = "ID of GatewaySubnet from network module"
-}
-
-variable "onprem_gateway_name" {
-  type = string
-}
-
-variable "onprem_public_ip" {
-  type = string
-  description = "On-prem VPN device public IP"
-}
-
-variable "onprem_address_space" {
-  type = list(string)
-  description = "On-prem CIDR ranges"
-}
-
-variable "shared_key" {
-  type        = string
-  description = "Pre-shared key for S2S VPN"
-  sensitive   = true
 }
 
 variable "vpn_sku" {
