77dec5314821e8c8e1f59dbda79761c99c59ddaa
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-01-28 12:59:18 +0530

    Refactor variable definitions in variables.tf

--- staticwebapp/variables.tf
+++ staticwebapp/variables.tf
@@ -1,33 +1,26 @@
 variable "name" {
-  description = "Azure Static Web App name"
-  type        = string
+  type = string
 }
 
 variable "resource_group_name" {
-  description = "Existing resource group name"
-  type        = string
+  type = string
 }
 
 variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "East US 2"
+  type = string
 }
 
 variable "sku_tier" {
-  description = "Free or Standard"
-  type        = string
-  default     = "Free"
+  type    = string
+  default = "Free"
 }
 
 variable "sku_size" {
-  description = "SKU size"
-  type        = string
-  default     = "Free"
+  type    = string
+  default = "Free"
 }
 
 variable "tags" {
-  description = "Resource tags"
-  type        = map(string)
-  default     = {}
+  type    = map(string)
+  default = {}
 }
