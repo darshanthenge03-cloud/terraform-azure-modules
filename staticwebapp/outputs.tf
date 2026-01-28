7bffb364cb675e3ffe9586130a1e9306c98a77a3
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-01-28 12:59:35 +0530

    Rename outputs and update values for static web app

--- staticwebapp/outputs.tf
+++ staticwebapp/outputs.tf
@@ -1,15 +1,12 @@
-output "id" {
-  description = "Static Web App resource ID"
-  value       = azurerm_static_web_app.this.id
+output "static_web_app_id" {
+  value = azurerm_static_web_app.this.id
 }
 
-output "default_hostname" {
-  description = "Default Static Web App hostname"
-  value       = azurerm_static_web_app.this.default_host_name
+output "static_web_app_url" {
+  value = azurerm_static_web_app.this.default_host_name
 }
 
-output "api_key" {
-  description = "Deployment API key"
-  value       = azurerm_static_web_app.this.api_key
-  sensitive   = true
+output "deployment_token" {
+  value     = azurerm_static_web_app.this.api_key
+  sensitive = true
 }
