bcde19a06ec185839b46bfbc4bcce2724e859dda
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-01-28 16:01:57 +0530

    Add Azure CDN Front Door resources configuration

--- frontdoorcdn/main.tf
+++ frontdoorcdn/main.tf
@@ -1,1 +1,45 @@
+resource "azurerm_cdn_frontdoor_profile" "this" {
+  name                = var.profile_name
+  resource_group_name = var.resource_group_name
+  sku_name            = var.sku
 
+  tags = var.tags
+}
+
+resource "azurerm_cdn_frontdoor_endpoint" "this" {
+  name                     = var.endpoint_name
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.this.id
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "this" {
+  name                     = "origin-group"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.this.id
+
+  load_balancing {
+    successful_samples_required = 3
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin" "this" {
+  name                          = "origin"
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.this.id
+  enabled                       = true
+
+  host_name          = var.origin_host
+  http_port          = 80
+  https_port         = 443
+  origin_host_header = var.origin_host
+}
+
+resource "azurerm_cdn_frontdoor_route" "this" {
+  name                          = "default-route"
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.this.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.this.id
+  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.this.id]
+
+  supported_protocols = ["Http", "Https"]
+  patterns_to_match   = ["/*"]
+
+  forwarding_protocol = "HttpsOnly"
+  https_redirect_enabled = true
+}
