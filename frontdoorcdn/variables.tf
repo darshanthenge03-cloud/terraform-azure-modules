22d8caba6f0246ba085d118be8740977c252913a
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-01-28 16:03:12 +0530

    Add variables for Azure Front Door configuration

--- frontdoorcdn/variables.tf
+++ frontdoorcdn/variables.tf
@@ -1,1 +1,25 @@
+variable "profile_name" {
+  type = string
+}
 
+variable "endpoint_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "sku" {
+  type    = string
+  default = "Standard_AzureFrontDoor"
+}
+
+variable "origin_host" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
