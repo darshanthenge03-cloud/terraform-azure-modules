557240bc35fdce465f4f54ae16aee7d12d033a4b
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-01-28 15:37:04 +0530

    Add Azure Storage Account resource configuration

--- storageaccount/main.tf
+++ storageaccount/main.tf
@@ -1,1 +1,16 @@
+resource "azurerm_storage_account" "this" {
+  name                     = var.name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
 
+  account_tier             = var.account_tier
+  account_replication_type = var.replication_type
+  account_kind             = var.account_kind
+
+  enable_https_traffic_only = true
+  min_tls_version           = "TLS1_2"
+
+  allow_blob_public_access = var.allow_blob_public_access
+
+  tags = var.tags
+}
