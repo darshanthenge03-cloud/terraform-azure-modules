aeb9a71ffc157b89cb60bb23f15d791dde1a8455
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-01-28 12:59:03 +0530

    Update main.tf

--- staticwebapp/main.tf
+++ staticwebapp/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_static_web_app" "this" {
   name                = var.name
-  location            = var.location
   resource_group_name = var.resource_group_name
+  location            = var.location
 
   sku_tier = var.sku_tier
   sku_size = var.sku_size
 
-  tags = local.common_tags
+  tags = var.tags
 }
