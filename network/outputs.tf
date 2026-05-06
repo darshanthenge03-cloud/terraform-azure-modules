07369310a505ee35bb1de35a00545341b991d1af
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-05-06 15:31:56 +0530

    Refactor output variables in outputs.tf

--- network/outputs.tf
+++ network/outputs.tf
@@ -1,31 +1,9 @@
 output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
+  value = azurerm_virtual_network.this.id
 }
 
-output "public_subnet_ids" {
+output "subnet_ids" {
   value = {
-    for k, s in azurerm_subnet.public : k => s.id
+    for k, s in azurerm_subnet.this : k => s.id
   }
-}
-
-output "private_subnet_ids" {
-  value = {
-    for k, s in azurerm_subnet.private : k => s.id
-  }
-}
-
-output "bastion_subnet_id" {
-  value = azurerm_subnet.bastion.id
-}
-
-output "public_nsg_id" {
-  value = azurerm_network_security_group.public.id
-}
-
-output "private_nsg_id" {
-  value = azurerm_network_security_group.private.id
-}
-
-output "gateway_subnet_id" {
-  value = azurerm_subnet.gateway.id
 }
