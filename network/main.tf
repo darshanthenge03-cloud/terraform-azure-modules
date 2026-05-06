4b83c2d8583db59ac32ee79f4be94ecce6f1379e
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-05-06 15:31:42 +0530

    Refactor Azure network resources for better structure

--- network/main.tf
+++ network/main.tf
@@ -1,65 +1,35 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-${var.resource_group_name}"
+resource "azurerm_virtual_network" "this" {
+  name                = var.vnet_name
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = [var.vnet_cidr]
+
+  tags = var.tags
 }
 
-resource "azurerm_subnet" "public" {
-  for_each = var.public_subnets
+resource "azurerm_subnet" "this" {
+  for_each = var.subnets
 
   name                 = each.key
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = [each.value]
 }
 
-resource "azurerm_subnet" "private" {
-  for_each = var.private_subnets
+resource "azurerm_network_security_group" "this" {
+  for_each = var.subnets
 
-  name                 = each.key
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [each.value]
-}
-
-resource "azurerm_subnet" "bastion" {
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [var.bastion_subnet_cidr]
-}
-
-resource "azurerm_network_security_group" "public" {
-  name                = "nsg-public"
+  name                = "nsg-${each.key}"
   location            = var.location
   resource_group_name = var.resource_group_name
+
+  tags = var.tags
 }
 
-resource "azurerm_network_security_group" "private" {
-  name                = "nsg-private"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+resource "azurerm_subnet_network_security_group_association" "this" {
+  for_each = var.subnets
+
+  subnet_id = azurerm_subnet.this[each.key].id
+
+  network_security_group_id = azurerm_network_security_group.this[each.key].id
 }
-
-resource "azurerm_subnet_network_security_group_association" "public" {
-  for_each = azurerm_subnet.public
-
-  subnet_id                 = each.value.id
-  network_security_group_id = azurerm_network_security_group.public.id
-}
-
-resource "azurerm_subnet_network_security_group_association" "private" {
-  for_each = azurerm_subnet.private
-
-  subnet_id                 = each.value.id
-  network_security_group_id = azurerm_network_security_group.private.id
-}
-
-resource "azurerm_subnet" "gateway" {
-  name                 = "GatewaySubnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [var.gateway_subnet_cidr]
-}
-
