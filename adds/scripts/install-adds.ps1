cd6445b52a47babcf8119960d7fd5c3bcf408f3e
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-05-07 15:28:32 +0530

    Fix formatting for safe mode password variable

--- adds/scripts/install-adds.ps1
+++ adds/scripts/install-adds.ps1
@@ -4,11 +4,11 @@ $script = @"
 
 Import-Module ADDSDeployment
 
-\$safeModePassword = ConvertTo-SecureString 'Network@1234' -AsPlainText -Force
+$safeModePassword = ConvertTo-SecureString 'Network@1234' -AsPlainText -Force
 
 Install-ADDSForest `
 -DomainName 'dalberg.local' `
--SafeModeAdministratorPassword \$safeModePassword `
+-SafeModeAdministratorPassword $safeModePassword `
 -InstallDNS `
 -Force
 
