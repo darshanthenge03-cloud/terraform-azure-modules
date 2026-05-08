426e6723146393676f32a31831a785e175054d98
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-05-08 18:11:23 +0530

    Add domain join extension for virtual machines

--- avd/main.tf
+++ avd/main.tf
@@ -115,6 +115,34 @@ resource "azurerm_windows_virtual_machine" "vm" {
 }
 
 ########################################
+# Domain Join Extension
+########################################
+
+resource "azurerm_virtual_machine_extension" "domain_join" {
+
+  count = var.session_host_count
+
+  name               = "domain-join-${count.index}"
+  virtual_machine_id = azurerm_windows_virtual_machine.vm[count.index].id
+
+  publisher            = "Microsoft.Compute"
+  type                 = "JsonADDomainExtension"
+  type_handler_version = "1.3"
+
+  settings = jsonencode({
+    Name    = var.domain_name
+    OUPath  = var.ou_path
+    User    = "${var.domain_name}\\${var.domain_user}"
+    Restart = "true"
+    Options = "3"
+  })
+
+  protected_settings = jsonencode({
+    Password = var.domain_password
+  })
+}
+
+########################################
 # AVD Registration Extension
 ########################################
 
