ad12cc32a87f624b63a50a29b49b561c8f6f369a
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-05-11 15:47:16 +0530

    Update PowerShell script for AD DS installation

--- adds/scripts/install-adds.ps1
+++ adds/scripts/install-adds.ps1
@@ -1,35 +1,14 @@
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 
-$script = @"
-
 Import-Module ADDSDeployment
 
 $safeModePassword = ConvertTo-SecureString 'Network@1234' -AsPlainText -Force
 
 Install-ADDSForest `
--DomainName 'dalberg.local' `
+-DomainName "dalberg.local" `
 -SafeModeAdministratorPassword $safeModePassword `
 -InstallDNS `
--Force
-
-"@
-
-$path = "C:\\promote-dc.ps1"
-
-Set-Content -Path $path -Value $script
-
-$action = New-ScheduledTaskAction `
--Execute "PowerShell.exe" `
--Argument "-ExecutionPolicy Bypass -File C:\\promote-dc.ps1"
-
-$trigger = New-ScheduledTaskTrigger -AtStartup
-
-Register-ScheduledTask `
--TaskName "PromoteDC" `
--Action $action `
--Trigger $trigger `
--User "SYSTEM" `
--RunLevel Highest `
--Force
+-Force `
+-NoRebootOnCompletion
 
 Restart-Computer -Force
