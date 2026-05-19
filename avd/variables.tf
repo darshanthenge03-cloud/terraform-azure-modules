d70a5c701c7fb82099e7aaf7167f4c38c6331aed
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-05-19 13:24:50 +0530

    Add and update variables in variables.tf

--- avd/variables.tf
+++ avd/variables.tf
@@ -1,19 +1,35 @@
+########################################
+# AVD Core
+########################################
+
 variable "host_pool_name" {}
 variable "app_group_name" {}
 variable "workspace_name" {}
+
 variable "resource_group_name" {}
 variable "location" {}
+
 variable "subnet_id" {}
 
+########################################
+# Session Hosts
+########################################
+
 variable "session_host_count" {
+  type    = number
   default = 1
 }
 
 variable "vm_name_prefix" {}
 
 variable "vm_size" {
+  type    = string
   default = "Standard_D4s_v5"
 }
+
+########################################
+# Credentials
+########################################
 
 variable "admin_username" {}
 
@@ -21,17 +37,52 @@ variable "admin_password" {
   sensitive = true
 }
 
+########################################
+# Host Pool Configuration
+########################################
+
 variable "host_pool_type" {
+  type    = string
   default = "Pooled"
 }
 
 variable "load_balancer_type" {
+  type    = string
   default = "DepthFirst"
 }
 
 variable "max_sessions" {
+  type    = number
   default = 10
 }
+
+########################################
+# VM Image
+########################################
+
+variable "image_publisher" {
+  type    = string
+  default = "MicrosoftWindowsDesktop"
+}
+
+variable "image_offer" {
+  type    = string
+  default = "windows-11"
+}
+
+variable "image_sku" {
+  type    = string
+  default = "win11-22h2-avd"
+}
+
+variable "image_version" {
+  type    = string
+  default = "latest"
+}
+
+########################################
+# Tags
+########################################
 
 variable "tags" {
   type    = map(string)
@@ -39,7 +90,7 @@ variable "tags" {
 }
 
 ########################################
-# Domain Join Variables
+# Domain Join
 ########################################
 
 variable "domain_name" {
