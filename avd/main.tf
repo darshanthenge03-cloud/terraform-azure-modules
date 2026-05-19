ac4dd504adf8dde0832f92265181f7025cd901c7
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-05-19 13:34:48 +0530

    Replace static os_disk_type with variable

--- avd/main.tf
+++ avd/main.tf
@@ -101,7 +101,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "StandardSSD_LRS"
+    storage_account_type = var.os_disk_type
   }
 
   source_image_reference {
