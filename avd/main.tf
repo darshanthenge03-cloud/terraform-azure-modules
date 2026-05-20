44b0b853b7a4dca6ed71956e6af3c9f2558c44a1
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-05-20 22:34:14 +0530

    Refactor AVD Terraform configuration for clarity

--- avd/main.tf
+++ avd/main.tf
@@ -3,6 +3,7 @@
 ########################################
 
 resource "azurerm_virtual_desktop_host_pool" "this" {
+
   name                = var.host_pool_name
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -19,6 +20,7 @@ resource "azurerm_virtual_desktop_host_pool" "this" {
 ########################################
 
 resource "azurerm_virtual_desktop_application_group" "this" {
+
   name                = var.app_group_name
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -34,6 +36,7 @@ resource "azurerm_virtual_desktop_application_group" "this" {
 ########################################
 
 resource "azurerm_virtual_desktop_workspace" "this" {
+
   name                = var.workspace_name
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -46,6 +49,7 @@ resource "azurerm_virtual_desktop_workspace" "this" {
 ########################################
 
 resource "azurerm_virtual_desktop_workspace_application_group_association" "this" {
+
   workspace_id         = azurerm_virtual_desktop_workspace.this.id
   application_group_id = azurerm_virtual_desktop_application_group.this.id
 }
@@ -55,6 +59,7 @@ resource "azurerm_virtual_desktop_workspace_application_group_association" "this
 ########################################
 
 resource "azurerm_virtual_desktop_host_pool_registration_info" "this" {
+
   hostpool_id     = azurerm_virtual_desktop_host_pool.this.id
   expiration_date = timeadd(timestamp(), "24h")
 }
@@ -64,12 +69,15 @@ resource "azurerm_virtual_desktop_host_pool_registration_info" "this" {
 ########################################
 
 resource "azurerm_network_interface" "nic" {
+
   count               = var.session_host_count
+
   name                = "${var.vm_name_prefix}-${format("%02d", count.index + 1)}-nic"
   location            = var.location
   resource_group_name = var.resource_group_name
 
   ip_configuration {
+
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
@@ -83,14 +91,16 @@ resource "azurerm_network_interface" "nic" {
 ########################################
 
 resource "azurerm_windows_virtual_machine" "vm" {
-  count               = var.session_host_count
-  name                = "${var.vm_name_prefix}-${format("%02d", count.index + 1)}"
 
-  computer_name       = "avd${count.index + 1}"
+  count = var.session_host_count
+
+  name          = "${var.vm_name_prefix}-${format("%02d", count.index + 1)}"
+  computer_name = "avd${count.index + 1}"
 
   resource_group_name = var.resource_group_name
   location            = var.location
-  size                = var.vm_size
+
+  size = var.vm_size
 
   admin_username = var.admin_username
   admin_password = var.admin_password
@@ -100,16 +110,18 @@ resource "azurerm_windows_virtual_machine" "vm" {
   ]
 
   os_disk {
+
     caching              = "ReadWrite"
     storage_account_type = var.os_disk_type
   }
 
   source_image_reference {
+
     publisher = var.image_publisher
     offer     = var.image_offer
     sku       = var.image_sku
     version   = var.image_version
-  } 
+  }
 
   tags = var.tags
 }
@@ -130,6 +142,7 @@ resource "azurerm_virtual_machine_extension" "domain_join" {
   type_handler_version = "1.3"
 
   settings = jsonencode({
+
     Name    = var.domain_name
     OUPath  = var.ou_path
     User    = "${var.domain_user}@${var.domain_name}"
@@ -138,6 +151,7 @@ resource "azurerm_virtual_machine_extension" "domain_join" {
   })
 
   protected_settings = jsonencode({
+
     Password = var.domain_password
   })
 }
@@ -148,9 +162,10 @@ resource "azurerm_virtual_machine_extension" "domain_join" {
 
 resource "azurerm_virtual_machine_extension" "avd_register" {
 
-  count                = var.session_host_count
-  name                 = "avd-register-${count.index}"
-  virtual_machine_id   = azurerm_windows_virtual_machine.vm[count.index].id
+  count = var.session_host_count
+
+  name               = "avd-register-${count.index}"
+  virtual_machine_id = azurerm_windows_virtual_machine.vm[count.index].id
 
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
@@ -162,12 +177,30 @@ resource "azurerm_virtual_machine_extension" "avd_register" {
   ]
 
   settings = jsonencode({
+
     commandToExecute = <<COMMAND
 powershell -ExecutionPolicy Unrestricted -Command "
+
 $token='${azurerm_virtual_desktop_host_pool_registration_info.this.token}';
-Invoke-WebRequest -Uri https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv -OutFile C:\\AVD-Agent.msi;
-Start-Process msiexec.exe -ArgumentList '/i C:\\AVD-Agent.msi /quiet REGISTRATIONTOKEN=$token' -Wait;
+
+Invoke-WebRequest `
+-Uri https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv `
+-OutFile C:\\AVD-Agent.msi;
+
+Invoke-WebRequest `
+-Uri https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH `
+-OutFile C:\\AVD-Bootloader.msi;
+
+Start-Process msiexec.exe `
+-ArgumentList '/i C:\\AVD-Agent.msi /quiet REGISTRATIONTOKEN='$token `
+-Wait;
+
+Start-Process msiexec.exe `
+-ArgumentList '/i C:\\AVD-Bootloader.msi /quiet' `
+-Wait;
+
 "
 COMMAND
+
   })
 }
