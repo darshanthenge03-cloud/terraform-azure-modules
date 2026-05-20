a6f78e6217ba1633124008d68f9305d3420b0b30
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-05-20 19:48:08 +0530

    Update main.tf

--- bastion/main.tf
+++ bastion/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_public_ip" "bastion" {
   resource_group_name = var.resource_group_name
 
   allocation_method = "Static"
-  sku               = "Standard"
+  sku               = "Basic"
 }
 
 resource "azurerm_bastion_host" "this" {
