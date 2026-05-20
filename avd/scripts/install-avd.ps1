3569b19af9a1e21bc34e29255da6042d06d506b3
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-05-20 23:01:02 +0530

    Create install-avd.ps1

---
+++ avd/scripts/install-avd.ps1
@@ -0,0 +1,19 @@
+param (
+    [string]$RegistrationToken
+)
+
+Invoke-WebRequest `
+-Uri "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv" `
+-OutFile "C:\AVD-Agent.msi"
+
+Invoke-WebRequest `
+-Uri "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH" `
+-OutFile "C:\AVD-Bootloader.msi"
+
+Start-Process msiexec.exe `
+-Wait `
+-ArgumentList "/i C:\AVD-Agent.msi /quiet REGISTRATIONTOKEN=$RegistrationToken"
+
+Start-Process msiexec.exe `
+-Wait `
+-ArgumentList "/i C:\AVD-Bootloader.msi /quiet"
