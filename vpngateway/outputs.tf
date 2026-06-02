329d1308e2a31b83bec89218f5948f0ecaee2455
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-06-02 17:40:39 +0530

    Update outputs.tf

--- vpngateway/outputs.tf
+++ vpngateway/outputs.tf
@@ -2,6 +2,14 @@ output "vpn_gateway_id" {
   value = azurerm_virtual_network_gateway.this.id
 }
 
+output "vpn_gateway_name" {
+  value = azurerm_virtual_network_gateway.this.name
+}
+
 output "vpn_public_ip" {
   value = azurerm_public_ip.vpn.ip_address
+}
+
+output "vpn_public_ip_id" {
+  value = azurerm_public_ip.vpn.id
 }
