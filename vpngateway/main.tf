31adab25dd02416260f77e9e4b91fdb608fb763f
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-06-02 18:58:38 +0530

    Update main.tf

--- vpngateway/main.tf
+++ vpngateway/main.tf
@@ -1,18 +1,16 @@
 resource "azurerm_public_ip" "vpn" {
-
-  name                = var.public_ip_name
+  name                = "pip-vpn-gateway"
   location            = var.location
   resource_group_name = var.resource_group_name
 
   allocation_method = "Static"
   sku               = "Standard"
 
-  tags = var.tags
+  zones = var.zones
 }
 
 resource "azurerm_virtual_network_gateway" "this" {
-
-  name                = var.vpn_gateway_name
+  name                = "vnet-gateway"
   location            = var.location
   resource_group_name = var.resource_group_name
 
@@ -25,14 +23,9 @@ resource "azurerm_virtual_network_gateway" "this" {
   enable_bgp    = false
 
   ip_configuration {
-
-    name = "gateway-ipconfig"
-
+    name                          = "vnet-gateway-ipconfig"
     public_ip_address_id          = azurerm_public_ip.vpn.id
     private_ip_address_allocation = "Dynamic"
-
-    subnet_id = var.gateway_subnet_id
+    subnet_id                     = var.gateway_subnet_id
   }
-
-  tags = var.tags
 }
