8add1e0bcbcce0833a40ba9ea406369b20b2ee2a
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-06-02 17:40:20 +0530

    Refactor VPN gateway configuration to use variables

--- vpngateway/main.tf
+++ vpngateway/main.tf
@@ -1,28 +1,38 @@
 resource "azurerm_public_ip" "vpn" {
-  name                = "pip-vpn-gateway"
+
+  name                = var.public_ip_name
   location            = var.location
   resource_group_name = var.resource_group_name
 
   allocation_method = "Static"
   sku               = "Standard"
+
+  tags = var.tags
 }
 
 resource "azurerm_virtual_network_gateway" "this" {
-  name                = "vnet-gateway"
+
+  name                = var.vpn_gateway_name
   location            = var.location
   resource_group_name = var.resource_group_name
 
   type     = "Vpn"
   vpn_type = "RouteBased"
-  sku      = var.vpn_sku
+
+  sku = var.vpn_sku
 
   active_active = false
   enable_bgp    = false
 
   ip_configuration {
-    name                          = "vnet-gateway-ipconfig"
+
+    name = "gateway-ipconfig"
+
     public_ip_address_id          = azurerm_public_ip.vpn.id
     private_ip_address_allocation = "Dynamic"
-    subnet_id                     = var.gateway_subnet_id
+
+    subnet_id = var.gateway_subnet_id
   }
+
+  tags = var.tags
 }
